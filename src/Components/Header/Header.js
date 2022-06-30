1be6cb6f2bb88ba2a4ef81da065bca48f0782931
Deepika <deepika2002mishra@gmail.com>
2022-06-30 17:54:26 +0530

    adding trollface

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './header.css'
-
+import trollface from '../../Images/pngegg.png'
 
 export default function Header(){
      return(
 <header className="header">
+<img className="trollface" src={trollface}/>
 <h3>Meme Generator</h3>
 </header>
 
