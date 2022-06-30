4c434651167f03bf64552bdf90129b93f93ca838
Deepika <deepika2002mishra@gmail.com>
2022-06-30 17:32:45 +0530

    Removed Warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React,{Component} from 'react';
+import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Meme from './Components/Meme/Meme';
